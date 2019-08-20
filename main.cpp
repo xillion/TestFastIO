3da951fe1df88c8a908d93f71d39259de67d95de
xillion <kurbanovd.s@gmail.com>
2019-08-21 00:36:04 +0300

    initial suppurt bus in out

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include "mbed.h"
 #include "bsp.h"
 
+typedef struct
+{
+    /* data */
+    uint8_t bit0 :1;
+    uint8_t bit1 :1;
+    uint8_t bit2 :1;
+    uint8_t bit3 :1;
+    uint8_t bit4 :1;
+    uint8_t bit5 :1;
+    uint8_t bit6 :1;
+    uint8_t bit7 :1;
+}bit_merge_t;
+
+
 FastInOut<COLLECT_LED> led1;
 FastInOut<CHARGE_LED> led2;
 
-
-
-
- 
 int main() 
 {
     while(1)
